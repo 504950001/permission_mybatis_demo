4217aed0591acb6b4417b3293971d6046f87394d
zemal <504950001@qq.com>
2017-07-31 18:14:27 +0800

    提交信息

---
+++ src/main/java/cn/kingfeeling/controller/TestController.java
@@ -0,0 +1,46 @@
+package cn.kingfeeling.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author zemal-tan
+ * @description
+ * @create 2017-07-31 17:13
+ */
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+
+    @RequestMapping("index")
+    @ResponseBody
+    public ModelAndView add(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+
+    @RequestMapping("index2")
+    public ModelAndView add2(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    @ResponseBody
+    public String welcome(){
+        return "test";
+    }
+
+    @RequestMapping(value = "test2", method = RequestMethod.GET)
+    public String welcome2(){
+        return "test";
+    }
+
+}
---
+++ target/springmvc_mybatis_demo/WEB-INF/test.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello test! in web-inf</h2>
+</body>
+</html>
